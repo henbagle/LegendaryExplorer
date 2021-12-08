a4da1f141a49c4e154587bbdd76f569ddf92f0ac
ME3Tweaks-LEC-Documentation <hen.bagle@gmail.com>
2021-12-08 04:06:52 +0000

    Update documentation [skip ci]

---
+++ nightly/html/dir_a240a62b4baa8a4606f83e666af7ae5d.js
@@ -0,0 +1,11 @@
+var dir_a240a62b4baa8a4606f83e666af7ae5d =
+[
+    [ "HuffmanCompression.cs", "_m_e2_m_e3_2_huffman_compression_8cs.html", [
+      [ "LegendaryExplorerCore.TLK.ME2ME3.HuffmanCompression", "class_legendary_explorer_core_1_1_t_l_k_1_1_m_e2_m_e3_1_1_huffman_compression.html", "class_legendary_explorer_core_1_1_t_l_k_1_1_m_e2_m_e3_1_1_huffman_compression" ]
+    ] ],
+    [ "ME2ME3TalkFile.cs", "_m_e2_m_e3_talk_file_8cs.html", [
+      [ "LegendaryExplorerCore.TLK.ME2ME3.TalkFile", "class_legendary_explorer_core_1_1_t_l_k_1_1_m_e2_m_e3_1_1_talk_file.html", "class_legendary_explorer_core_1_1_t_l_k_1_1_m_e2_m_e3_1_1_talk_file" ],
+      [ "LegendaryExplorerCore.TLK.ME2ME3.TalkFile.TLKHeader", "struct_legendary_explorer_core_1_1_t_l_k_1_1_m_e2_m_e3_1_1_talk_file_1_1_t_l_k_header.html", "struct_legendary_explorer_core_1_1_t_l_k_1_1_m_e2_m_e3_1_1_talk_file_1_1_t_l_k_header" ],
+      [ "LegendaryExplorerCore.TLK.ME2ME3.TalkFile.HuffmanNode", "struct_legendary_explorer_core_1_1_t_l_k_1_1_m_e2_m_e3_1_1_talk_file_1_1_huffman_node.html", "struct_legendary_explorer_core_1_1_t_l_k_1_1_m_e2_m_e3_1_1_talk_file_1_1_huffman_node" ]
+    ] ]
+];
---
+++ nightly/html/namespace_legendary_explorer_core_1_1_t_l_k.js
@@ -0,0 +1,9 @@
+var namespace_legendary_explorer_core_1_1_t_l_k =
+[
+    [ "ME1", "namespace_legendary_explorer_core_1_1_t_l_k_1_1_m_e1.html", "namespace_legendary_explorer_core_1_1_t_l_k_1_1_m_e1" ],
+    [ "ME2ME3", "namespace_legendary_explorer_core_1_1_t_l_k_1_1_m_e2_m_e3.html", "namespace_legendary_explorer_core_1_1_t_l_k_1_1_m_e2_m_e3" ],
+    [ "LE2TalkFiles", "class_legendary_explorer_core_1_1_t_l_k_1_1_l_e2_talk_files.html", "class_legendary_explorer_core_1_1_t_l_k_1_1_l_e2_talk_files" ],
+    [ "LE3TalkFiles", "class_legendary_explorer_core_1_1_t_l_k_1_1_l_e3_talk_files.html", "class_legendary_explorer_core_1_1_t_l_k_1_1_l_e3_talk_files" ],
+    [ "ME2TalkFiles", "class_legendary_explorer_core_1_1_t_l_k_1_1_m_e2_talk_files.html", "class_legendary_explorer_core_1_1_t_l_k_1_1_m_e2_talk_files" ],
+    [ "ME3TalkFiles", "class_legendary_explorer_core_1_1_t_l_k_1_1_m_e3_talk_files.html", "class_legendary_explorer_core_1_1_t_l_k_1_1_m_e3_talk_files" ]
+];
